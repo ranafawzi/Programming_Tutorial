6a882267f1ac6eebdb16f5b8cb5885d52bbea24f
ranafawzi <ranafawzi85@yahoo.com>
2016-11-18 16:21:44 +0100

    Structure assignment is done

--- Structures/src/AssignGrade.java
+++ Structures/src/AssignGrade.java
@@ -2,14 +2,42 @@ import java.util.Scanner;
 
 //if-else
 
+
+
 public class AssignGrade {
+	
+
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		double x = scan.nextDouble();
+		System.out.println(x);
 		
-		//todo: when executing the program you will be ask to write a score.
+		//todo: when executing the program you will be ask to write a score.		
 		//The program converts the score then into a grade and prints that grade (1.0, 1.3, 1.7, ...)
-		//Note that the student passed when the student gets a score of 50.0 points (4.0)
+		//Note that the student passed when the student gets a score of 50.0 points (4.0)		
+		
+		if(x >= 0 && x < 50)			
+		{
+			
+			System.out.println("Score = " + x + " Grade = 5.0  "  );
+		
+		} 			
+		else if(x >= 50 && x < 65)
+		{
+			
+			System.out.println("Score = " + x + " Grade = 4.0  "  );
+		} 
+		else if (x >= 65 && x < 80){
+			
+			System.out.println("Score = " + x + " Grade = 3.3"  );
+		}
+		else if (x >= 80 && x < 90){
+			System.out.println("Score = " + x + " Grade = 2.3"  );
+		}
+		else if (x >= 90 && x < 100){
+			System.out.println("Score = " + x + " Grade = 1.3"  );
+		}			
 		
 	}
 }
--- Structures/src/EvenOdd.java
+++ Structures/src/EvenOdd.java
@@ -6,10 +6,23 @@ public class EvenOdd {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		int x = scan.nextInt();
+		int res;
+		
+		for(int i=0; i<= x; i++)
+		{
+		  res= i % 2;
+			if( res != 0)
+			{
+				System.out.println("The odd numbers are" + i );
+			}
+			
+		}
 		
 		//todo: The program takes a number as input.
 		//It then goes through all integer values till reaching that value and prints all the odd ones.
 		
+	scan.close();	
 	}
 	
 }
