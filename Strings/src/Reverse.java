3c2ce8840cf8fbc537102bfe3233ee296bbcc2a1
ranafawzi <ranafawzi85@yahoo.com>
2016-11-25 15:23:58 +0100

    Strings Reversing is done

--- Strings/src/Reverse.java
+++ Strings/src/Reverse.java
@@ -6,9 +6,16 @@ public class Reverse {
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		String input="", rev="";
 		
 		//todo: take an String as input and reverse it. Then print it.
+		input = scan.nextLine();
+		rev = new StringBuffer(input).reverse().toString();
+        System.out.println("The orignal Stringd:"+input);
+        System.out.println("String after reverse:"+rev);
 		
+		
+    	scan.close();		
 	}
 
 }
