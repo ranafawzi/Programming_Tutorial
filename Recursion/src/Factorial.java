b72b85ab8daee9e990a65f516c02c9d730ec28a4
ranafawzi <ranafawzi85@yahoo.com>
2016-11-25 15:26:58 +0100

    Recursion is done

--- Recursion/src/Factorial.java
+++ Recursion/src/Factorial.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
 
 public class Factorial {
 	//todo: implement a recursive functions whicht gives you the factorial value of a given number
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int x = scan.nextInt();
+		 System.out.println(factorial(x));
+    }
+	
+	 private static long factorial(int num) {
+		 
+	        if(num<0)  throw new IllegalArgumentException("The number must be >= 0"); 
+	        return num==0||num==1? 1:num*factorial(num-1);
+	    }
+	
+	
 }
--- Recursion/src/Fibonacci.java
+++ Recursion/src/Fibonacci.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 
 public class Fibonacci {
 	//todo: implement a recursive function which takes an integer value as inputs and outputs the corresponding Fibonacci number.
 	//if you don't know what Fibonacci numbers are you can read it here: https://en.wikipedia.org/wiki/Fibonacci_number
+
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int x = scan.nextInt();
+		 System.out.println("Faibonacci " + x + " =  " + fibonacci(x));
+    }
+	
+	 private static long fibonacci(int num) {
+		 
+	        if(num<0)  throw new IllegalArgumentException("The number must be >= 0"); 
+	        return num==0||num==1? 1: fibonacci(num-1)+fibonacci(num-2);
+	    }
+	
+
+
+
 }
